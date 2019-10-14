c31f4b148dab2f3259bc7949878fb46ca6339515
Zachary Mattor <zmattor@gmail.com>
2019-10-13 20:45:18 -0400

    it compiles!

--- src/main.cpp
+++ src/main.cpp
@@ -35,26 +35,26 @@ static const uint8_t icosahedron_neighbors[20][3] = {
 };
 
 static const uint8_t map_icosahedron_hardware[] = {
-  1,  // 1
-  5,  // 2
-  6,  // 3
-  7,  // 4
-  8,  // 5
-  3,  // 6
-  4,  // 7
-  19, // 8
-  20, // 9
-  15, // 10
-  14, // 11
-  13, // 12
-  9,  // 13
-  10, // 14
-  2,  // 15
-  17, // 16
-  18, // 17
-  16, // 18
-  12, // 19
-  11  // 20
+  0,  // 1
+  4,  // 2
+  5,  // 3
+  6,  // 4
+  7,  // 5
+  2,  // 6
+  3,  // 7
+  18, // 8
+  19, // 9
+  14, // 10
+  13, // 11
+  12, // 12
+  8,  // 13
+  9,  // 14
+  1,  // 15
+  16, // 16
+  17, // 17
+  15, // 18
+  11, // 19
+  10  // 20
 };
 
 
@@ -68,11 +68,11 @@ uint8_t current = 1;
 uint8_t prev = 5;
 
 uint8_t next_surface_id(uint8_t previous_id, uint8_t current_id, Direction dir) {
-
+  return 4;
 }
 
 uint8_t surface_id_to_led_id(uint8_t surface_id) {
-  return map_icosahedron_hardware[surface_id - 1]-1;
+  return map_icosahedron_hardware[surface_id - 1];
 }
 
 void setup() {
@@ -83,8 +83,9 @@ void setup() {
 void loop() {
   strip.clear();
 
+  uint8_t next = next_surface_id(prev, current, steps[step % 5]);
 
-  strip.setPixelColor(, strip.Color(0, 0, 0, 10));
+  strip.setPixelColor(surface_id_to_led_id(next), strip.Color(0, 50, 0, 0));
 
   strip.show();
 
